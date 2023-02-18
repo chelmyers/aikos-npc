71359f7079763744e91c9acb02cb8d100a0c8510
Chelsea <chelsea@Chelseas-MacBook-Pro-2.local>
2023-02-18 15:53:37 -0800

    add saved npc tab and localstorage logic

--- a/script.js
+++ a/script.js
@@ -1,47 +1,85 @@
-let data, fields;
+/* HEY YOU, 
+could you not judge my shitty code, yah snoop? Jeeeez. 
+- xoxoxo chelsea */
+
+
+let data, fields, saves;
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    // Your code to run since DOM is loaded and ready
 
     console.log('DOM ready');
 
-    Papa.parse('https://docs.google.com/spreadsheets/d/1PB8o8w9kHG5eJ9SatDOFxfI3W6ELl2wbmX8lnwS6Xk0/gviz/tq?tqx=out:csv&sheet=data', {
-        download: true,
-        header: true,
-        complete: results => {
-            console.log(results);
-            fields = results.meta.fields;
-            data = filterData(results.data, results.meta.fields);
-            console.log(data);
+    //set up localstorage
+    if(localStorage.saves == null) {
+        saves = [];
+        localStorage.saves = JSON.stringify(saves);
+    } else {
+        if(localStorage.saves) {
+            saves = JSON.parse(localStorage.saves);
+        } else {
+            saves = [];
         }
-    })
+        generateSavedNPCs(saves); 
+    }
+
+    //Google sheet data
+    Papa.parse('https://docs.google.com/spreadsheets/d/1PB8o8w9kHG5eJ9SatDOFxfI3W6ELl2wbmX8lnwS6Xk0/gviz/tq?tqx=out:csv&sheet=data', { download: true,  header: true, complete: results => { fields = results.meta.fields; data = filterData(results.data, results.meta.fields); }})
+
+    //Local data
+    // Papa.parse('./a/data/data.csv', { download: true,  header: true, complete: results => {  fields = results.meta.fields; data = filterData(results.data, results.meta.fields);  }})
 
 
-    // Papa.parse('./a/data/data.csv', {
-    //     download: true,
-    //     header: true,
-    //     complete: results => {
-    //         console.log(results);
-    //         fields = results.meta.fields;
-    //         data = filterData(results.data, results.meta.fields);
-    //         console.log(data);
-    //     }
-    // })
+    //listen for saving npcs
+    let generate = document.querySelector('.generate-content');
+    generate.addEventListener('click', function (event) {
+        if (event.target.classList.contains('save-npc')) {
+            let savedNPC = event.target.parentElement;
+            saves.push(savedNPC.innerHTML );
+            localStorage.saves = JSON.stringify(saves);
+            generateSavedNPCs(saves); 
+            event.target.classList.add('saved');
+        }
+      });
+
+    //listen for tabs
+    let tabs = document.querySelector('.tabs-list');
+    tabs.addEventListener('click', function (event) {
+        if (event.target.classList.contains('tabs-btn')) {
+            document.querySelector('.active').classList.remove('active');
+            event.target.classList.add('active');
+            let target = event.target.dataset.id;
+
+            if( target == 'saves') {
+                document.querySelector('.tabs-container').classList.add('show-saves');
+            } else if (target == 'generate') {
+                document.querySelector('.tabs-container').classList.remove('show-saves');
+            }
+          
+        }
+      });
 
 });
 
 
-function generate () {
-    
 
-    document.querySelector(".generate").innerHTML = "";
+//genrate new NPCs
+function generateNPCs () {
     
-    let n = 10;
-    let options;
+    //changes tabs to generate
+    document.querySelector('.tabs-container').classList.remove('show-saves');
+    document.querySelector('.active').classList.remove('active');
+    document.querySelector("[data-id='generate']").classList.add('active');
+
+    //clear tab
+    document.querySelector(".generate-content").innerHTML = "";
+
+    //grab n
+    let n = document.querySelector('#number').value;
     
+    //for n
     for (var i = 0; i < n; i++) {
 
-        // let prevNums = [];
+        //build npc object
         let npc = {};
 
         for(j = 0; j < fields.length; j++) {
@@ -50,53 +88,67 @@ function generate () {
 
             //Set data
             let select = randomNumber(max);
-            // let prevUsed = prevNums.includes(select);
-
-            // do { 
-            //     select = randomNumber(max);
-            //     prevUsed = prevNums.includes(select);
-            // } while (prevUsed == true);
-
-            // prevNums.push(select);
-
-            //grab random data
-            // Insert into page
-
             
-
             npc[fields[j]] = data[fields[j]][select]
-
-            let option = data[fields[j]][select];
         }
 
-        
-
+        //clean up dndbeyond id
         statsName = npc.stats.split(/-(.*)/s)
         npc.statsName = statsName[1];
 
-        console.log(npc);
-
-        let npcFormatted = `<ul><li class="title">${npc.firstname} ${npc.lastname}</li>`
+        //format html for npc
+        let npcFormatted = `<ul><li class="title save-npc">${npc.firstname} ${npc.lastname}</li>`
         npcFormatted += `<li>${npc.race} &middot; ${npc.age} &middot; <a href="https://www.dndbeyond.com/monsters/${npc.stats}" target="_blank">${npc.statsName}</a></li>`
         npcFormatted += `<li>${npc.build} &middot; ${npc.detail}</li>`
-        npcFormatted += `<li>${npc.personality}</li><ul>`
+        npcFormatted += `<li>${npc.personality}</li></ul>`
 
+        //add npc to dom
         let html = document.createElement('div');
         html.className = 'npc';
-        console.log(options);
         html.innerHTML = npcFormatted;
-        document.querySelector(".generate").append(html);
-
-
+        document.querySelector(".generate-content").append(html);
     }
      
 }
 
+//generate saved npcs in saves tab
+function generateSavedNPCs(saves){
 
+    //clear tab
+    document.querySelector(".saves-content").innerHTML = "";
+
+    //loop through saved npcs and add to dom
+    //or show empty state
+    if(saves.length > 0) {
+        for(i = 0; i < saves.length; i++) {
+            let npcFormatted = `<ul>${saves[i]}</ul>`;
+            let html = document.createElement('div');
+            html.className = 'npc';
+            html.innerHTML = npcFormatted;
+            document.querySelector(".saves-content").append(html);
+        }
+        
+    } else {
+        document.querySelector(".saves-content").innerHTML = '<p>Click NPC names under the "New" tab to save them here.</p>';
+    }
+
+}
+
+//clear saved npcs and tabs
+function clearSaves(){
+    if (window.confirm("Are you sure you want to clear your saved NPCs?")) {
+        localStorage.clear();
+        saves=[];
+        generateSavedNPCs([]);
+      }
+}
+
+//random number for npc data selection
 function randomNumber(max) {
     return Math.floor(Math.random() * max);
 }
 
+//parse csv
 function filterData(csv, headers){
     let filteredJSON = {};
     let filteredArray = [];
@@ -121,6 +173,5 @@ function filterData(csv, headers){
         filteredJSON[label] = filteredArray[i];
     }
 
-//    console.log(filteredJSON);
    return filteredJSON;
 }
